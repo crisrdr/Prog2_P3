3648faadeefd8980323639e1857c965ba4ca72bb
crisrdr <cristina.rodriguezdelosrios@estudiante.uam.es>
2022-04-05 12:28:47 +0200

    provisional

--- p3_e2.c
+++ p3_e2.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "map.h"
 
 int main (int argc, char *argv[]){
+    FILE *f;
+    Map *map = NULL;
     int n = 0;
 
     if (argc < 2){
@@ -12,7 +15,34 @@ int main (int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    n = atoi(argv[1]);        /* Asignación al primer argumento recibido al número de puntos a generar */
+    if ((f = fopen(argv[1], "r")) == NULL){   /* Argumento en posicion 1, nombre del fichero de texto */
+        fprintf(stdout,"Opening file failed\n");
+        return EXIT_FAILURE;
+    }
+
+    if ((map = map_readFromFile(f)) == NULL){ /* Creación del mapa a partir del fichero */
+        fprintf(stdout,"Run failed\n");
+        fclose(f);
+        return EXIT_FAILURE;
+    }
+    fclose(f);
+
+    /* Impresión del mapa leido */
+    fprintf(stdout, "Maze:\n");
+    if (map_print (stdout, map) == -1){
+        fprintf(stdout,"Run failed\n");
+        map_free(map);
+        return EXIT_FAILURE;
+    }
+    fprintf(stdout, "\n");
+
+    /* Salida de la búsqueda en anchura */
+    fprintf(stdout, "BFS:\n");
+    if (map_bfs(stdout, map) == NULL){
+        map_free(map);
+        return EXIT_FAILURE;
+    }
+    map_free(map);
 
     return EXIT_SUCCESS;
 }
